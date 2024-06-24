a3c14c6383b131c9d9a8439caae82856c3846fe6
Erick <82246694+DanerickDan@users.noreply.github.com>
2024-06-23 21:05:03 -0400

    Adding a custom component to the presesentation layer

--- LogForm/MainForm.Designer.cs
+++ LogForm/MainForm.Designer.cs
@@ -28,136 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
-            iconButton1 = new FontAwesome.Sharp.IconButton();
-            iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
-            groupBox1 = new GroupBox();
-            groupBox2 = new GroupBox();
-            panel2 = new Panel();
-            groupBox3 = new GroupBox();
-            panel3 = new Panel();
-            panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)iconPictureBox1).BeginInit();
-            groupBox1.SuspendLayout();
-            groupBox2.SuspendLayout();
-            groupBox3.SuspendLayout();
+            customPanel2 = new CustomComponents.MainFormComponents.CustomPanel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
             SuspendLayout();
             // 
-            // panel1
+            // customPanel2
             // 
-            panel1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            panel1.BackColor = Color.White;
-            panel1.Controls.Add(iconButton1);
-            panel1.Controls.Add(iconPictureBox1);
-            panel1.Location = new Point(3, 3);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(96, 509);
-            panel1.TabIndex = 0;
-            panel1.Paint += panel1_Paint;
+            customPanel2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            customPanel2.BackColor = Color.White;
+            customPanel2.BorderColor = Color.White;
+            customPanel2.Location = new Point(57, 1);
+            customPanel2.Name = "customPanel2";
+            customPanel2.Radius = 15;
+            customPanel2.Size = new Size(786, 28);
+            customPanel2.TabIndex = 4;
+            customPanel2.Thickness = 5F;
             // 
-            // iconButton1
+            // flowLayoutPanel1
             // 
-            iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
-            iconButton1.IconColor = Color.Black;
-            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            iconButton1.Location = new Point(3, 83);
-            iconButton1.Name = "iconButton1";
-            iconButton1.Size = new Size(75, 23);
-            iconButton1.TabIndex = 1;
-            iconButton1.Text = "iconButton1";
-            iconButton1.UseVisualStyleBackColor = true;
-            // 
-            // iconPictureBox1
-            // 
-            iconPictureBox1.BackColor = Color.Silver;
-            iconPictureBox1.ForeColor = SystemColors.ControlText;
-            iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.None;
-            iconPictureBox1.IconColor = SystemColors.ControlText;
-            iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            iconPictureBox1.Location = new Point(11, 14);
-            iconPictureBox1.Name = "iconPictureBox1";
-            iconPictureBox1.Size = new Size(51, 32);
-            iconPictureBox1.TabIndex = 0;
-            iconPictureBox1.TabStop = false;
-            // 
-            // groupBox1
-            // 
-            groupBox1.Controls.Add(panel1);
-            groupBox1.Location = new Point(-2, 2);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(102, 515);
-            groupBox1.TabIndex = 1;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
-            // 
-            // groupBox2
-            // 
-            groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            groupBox2.Controls.Add(panel2);
-            groupBox2.Location = new Point(96, -17);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(731, 50);
-            groupBox2.TabIndex = 2;
-            groupBox2.TabStop = false;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.White;
-            panel2.Dock = DockStyle.Top;
-            panel2.Location = new Point(3, 19);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(725, 28);
-            panel2.TabIndex = 0;
-            // 
-            // groupBox3
-            // 
-            groupBox3.Controls.Add(panel3);
-            groupBox3.Location = new Point(137, 39);
-            groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(677, 458);
-            groupBox3.TabIndex = 3;
-            groupBox3.TabStop = false;
-            // 
-            // panel3
-            // 
-            panel3.BackColor = Color.White;
-            panel3.Dock = DockStyle.Fill;
-            panel3.Location = new Point(3, 19);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(671, 436);
-            panel3.TabIndex = 0;
-            panel3.Paint += panel3_Paint;
+            flowLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            flowLayoutPanel1.BackColor = Color.White;
+            flowLayoutPanel1.Location = new Point(1, 1);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(50, 514);
+            flowLayoutPanel1.TabIndex = 5;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(215, 217, 221);
-            ClientSize = new Size(826, 509);
-            Controls.Add(groupBox3);
-            Controls.Add(groupBox2);
-            Controls.Add(groupBox1);
+            ClientSize = new Size(834, 513);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(customPanel2);
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MainForm";
             Load += MainForm_Load;
-            panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)iconPictureBox1).EndInit();
-            groupBox1.ResumeLayout(false);
-            groupBox2.ResumeLayout(false);
-            groupBox3.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Panel panel1;
-        private GroupBox groupBox1;
-        private GroupBox groupBox2;
-        private Panel panel2;
-        private FontAwesome.Sharp.IconButton iconButton1;
-        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
-        private GroupBox groupBox3;
-        private Panel panel3;
+        private CustomComponents.MainFormComponents.CustomPanel customPanel1;
+        private CustomComponents.MainFormComponents.CustomPanel customPanel2;
+        private FlowLayoutPanel flowLayoutPanel1;
     }
 }
